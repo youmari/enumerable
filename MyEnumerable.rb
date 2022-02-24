c73cf87f411ae9603944ba11a4707cab70890f4d
youmari <youmari300@gmail.com>
2022-02-24 09:48:50 +0100

    Add all and any method

---
+++ MyEnumerable.rb
@@ -0,0 +1,14 @@
+
+module MyEnumerable
+
+  def all?
+    each { |num| return false unless yield(num) }
+    true
+  end
+
+  def any?
+    each { |num| return true if yield (num) }
+    false
+  end
+  
+end
---
+++ Mylist.rb
@@ -0,0 +1,23 @@
+require_relative 'MyEnumerable'
+
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&block)
+   @list.each(&block) 
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+puts list.all? {|e| e < 5}
+# => true
+puts list.all? {|e| e > 5}
+# => false
+puts list.any? {|e| e == 2}
+# => true
+puts list.any? {|e| e == 5}
+# => false
